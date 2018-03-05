1b35cfda76d801bd359874afff4baab32b2b879a
Brittany Morris <brittmmendez@gmail.com>
2018-03-05 19:59:22 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :users
   resources :attractions
 
-<<<<<<< HEAD
+
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-=======
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
->>>>>>> 05d078b8339d6a535cec4342fadcd6eb49a9f7bd
   post '/logout' => 'sessions#destroy'
 
 
